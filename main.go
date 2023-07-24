19c3ad15b9cdbbc0130be8c8d6cb064e893c7e5f
Thomas Olympio <86328090+thomazera@users.noreply.github.com>
2023-07-24 02:33:47 -0300

    Update main.go

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 
 	fmt.Println("Ponto de partida")
 
+	//Caso o app.go gere algum erro o codigo abaixo irá apresentar o que ocasionou o erro.
 	buscaApp := app.Gerar()
 	if erro := buscaApp.Run(os.Args); erro != nil {
 		log.Fatal(erro)
